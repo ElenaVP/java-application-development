2320d73d3be65425fc4c71370c46749a55f8d14b
ElenaVP <akulaakula@mail.ru>
2022-04-21 10:25:23 +0300

    Stateful decorator logger task: int accumulator

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -4,9 +4,28 @@ import static com.acme.dbo.txlog.ConsolePrinter.printToConsole;
 import static com.acme.dbo.txlog.Decorator.decorateMessage;
 
 public class Facade {
+    public static int intAccumulator;
+    public static String lastUsedType;
+
+    public static boolean checkIfTypeChanged(String currentType) {
+        if  ((lastUsedType == null) || (currentType.equals(lastUsedType))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void flushInteger () {
+        printToConsole(Integer.valueOf(intAccumulator).toString());
+        intAccumulator = 0;
+    }
 
     public static void log(int message) {
-        printToConsole(decorateMessage(message));
+        if (message == 0) {
+            flushInteger();
+        }
+        intAccumulator = intAccumulator + message;
+        lastUsedType="int";
     }
 
     public static void log(byte message) {
@@ -18,7 +37,11 @@ public class Facade {
     }
 
     public static void log(String message) {
-        printToConsole(decorateMessage(message));
+        if (checkIfTypeChanged("String")) {
+            flushInteger();
+        };
+        printToConsole(message);
+        lastUsedType="String";
     }
 
     public static void log(boolean message) {
--- src/test/java/com/acme/dbo/txlog/iteration02/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration02/LoggerTest.java
@@ -1,8 +1,10 @@
 package com.acme.dbo.txlog.iteration02;
 
+import com.acme.dbo.txlog.Facade;
 import com.acme.dbo.txlog.SysoutCaptureAndAssertionAbility;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -21,8 +23,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     //endregion
 
 
-    /*
-    TODO: implement Logger solution to match specification as tests
+
 
     @Test
     public void shouldLogSequentIntegersAsSum() throws IOException {
@@ -32,56 +33,15 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         Facade.log(2);
         Facade.log("str 2");
         Facade.log(0);
+        //Facade.flushInteger();
         //endregion
 
         //region then
         assertSysoutEquals(
-            "str 1\n" +
-            "3\n" +
-            "str 2\n" +
-            "0\n"
-        );
-        //endregion
-    }
-
-    @Test
-    public void shouldLogCorrectlyIntegerOverflowWhenSequentIntegers() {
-        //region when
-        Facade.log("str 1");
-        Facade.log(10);
-        Facade.log(Integer.MAX_VALUE);
-        Facade.log("str 2");
-        Facade.log(0);
-        //endregion
-
-        //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "10\n" +
-            Integer.MAX_VALUE + "\n" +
-            "str 2\n" +
-            "0\n"
-        );
-        //endregion
-    }
-
-    @Test
-    public void shouldLogCorrectlyByteOverflowWhenSequentBytes() {
-        //region when
-        Facade.log("str 1");
-        Facade.log((byte)10);
-        Facade.log((byte)Byte.MAX_VALUE);
-        Facade.log("str 2");
-        Facade.log(0);
-        //endregion
-
-        //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "10\n" +
-            Byte.MAX_VALUE + "\n" +
-            "str 2\n" +
-            "0\n"
+            "str 1\r\n" +
+            "3\r\n" +
+            "str 2\r\n" +
+            "0\r\n"
         );
         //endregion
     }
@@ -101,14 +61,59 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutEquals(
-            "str 1\n" +
-            "str 2 (x2)\n" +
-            "0\n" +
-            "str 2\n" +
-            "str 3 (x3)\n"
+                "str 1" + System.lineSeparator() +
+                        "str 2 (x2)" + System.lineSeparator() +
+                        "0\r\n" +
+                        "str 2\r\n" +
+                        "str 3 (x3)\r\n"
         );
         //endregion
     }
+
+    /*
+    TODO: implement Logger solution to match specification as tests
+    @Test
+    public void shouldLogCorrectlyIntegerOverflowWhenSequentIntegers() {
+        //region when
+        Facade.log("str 1");
+        Facade.log(10);
+        Facade.log(Integer.MAX_VALUE);
+        Facade.log("str 2");
+        Facade.log(0);
+        //endregion
+
+        //region then
+        assertSysoutEquals(
+            "str 1\r\n" +
+            "10\r\n" +
+            Integer.MAX_VALUE + "\r\n" +
+            "str 2\r\n" +
+            "0\r\n"
+        );
+        //endregion
+    }
+
+    @Test
+    public void shouldLogCorrectlyByteOverflowWhenSequentBytes() {
+        //region when
+        Facade.log("str 1");
+        Facade.log((byte)10);
+        Facade.log((byte)Byte.MAX_VALUE);
+        Facade.log("str 2");
+        Facade.log(0);
+        //endregion
+
+        //region then
+        assertSysoutEquals(
+            "str 1\r\n" +
+            "10\r\n" +
+            Byte.MAX_VALUE + "\r\n" +
+            "str 2\r\n" +
+            "0\r\n"
+        );
+        //endregion
+    }
+
 
     */
 }
